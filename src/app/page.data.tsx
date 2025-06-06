db0fa69debeb832df6255011d01f58fbaa5654cc
Alfonsus Ardani <alfonsusac@gmail.com>
2025-06-07 02:16:28 +0700

    fix: add missing newline at end of file in page.data.tsx

--- src/app/page.data.tsx
+++ src/app/page.data.tsx
@@ -14,5 +14,6 @@ export const getSiteMetadata = async (urlinput: string) => {
   } catch (error) {
     throw new AppError('getPageData', undefined, undefined, undefined, error)
   }
+}
 
 export type SiteMetadata = Awaited<ReturnType<typeof getSiteMetadata>>
